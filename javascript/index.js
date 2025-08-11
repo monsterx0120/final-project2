b05765c4afc538c5ae0a4512ba4b61f493e4d463
monsterx0120 <hamoudy0120@gmail.com>
2025-08-12 02:24:17 +0300

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+let data2=document.getElementById("htmldata")
+let html=`<h3>My name is Mohamed hussein badry and i am 12 years old.And i work as a <strong> developer</strong></h3>`
+function data() {
+    data2.innerHTML=html;
+}
+
